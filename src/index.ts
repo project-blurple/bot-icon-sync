8914283e34bb6529aec1641d6222eb5b12719fe3
Promise <me@promise.solutions>
2022-03-30 23:03:13 +0200

    Update linting config

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import "dotenv/config";
-import {existsSync, readFileSync} from "fs";
-import {join} from "path";
+import { existsSync, readFileSync } from "fs";
+import { join } from "path";
 import type request from "superagent";
 import superagent from "superagent";
 
-const {NAME = "", TOKEN = ""} = process.env;
+const { NAME = "", TOKEN = "" } = process.env;
 if (!NAME || !TOKEN) throw new Error("Missing environment variables");
 
 const lightPath = join(__dirname, "../icons", `${NAME} Light.png`);
@@ -17,7 +17,7 @@ superagent.patch("https://discord.com/api/v9/users/@me")
   .set("User-Agent", "Project Blurple Bot Image Synchronization (promise@projectblurple.com, 2.0.0)")
   .set("Authorization", `Bot ${TOKEN}`)
   .set("Content-Type", "application/json")
-  .send({avatar: `data:image/png;base64,${base64Image}`})
+  .send({ avatar: `data:image/png;base64,${base64Image}` })
   .then(() => { console.log("Successfully updated avatar"); })
   .catch((err: request.ResponseError) => {
     console.error("Failed to update avatar:", err.response?.text);

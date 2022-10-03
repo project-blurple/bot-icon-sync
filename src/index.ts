e43e8911b4caf2793c35bd18344a6e48b81329d3
Glenn <me@promise.solutions>
2022-10-03 17:45:43 +0000

    Use discord-api-types because I am a sheep

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { existsSync, readFileSync } from "fs";
 import { join } from "path";
 import type request from "superagent";
 import superagent from "superagent";
+import { RESTErrorData, RESTPatchAPICurrentUserJSONBody, RESTPatchAPICurrentUserResult, RouteBases, Routes } from "discord-api-types/v10";
 
 const { NAME = "", TOKEN = "" } = process.env;
 if (!NAME || !TOKEN) throw new Error("Missing environment variables");
@@ -13,13 +14,25 @@ if (!existsSync(lightPath) || !existsSync(darkPath)) throw new Error("Missing ic
 
 const base64Image = readFileSync(new Date().getHours() < 12 ? lightPath : darkPath, "base64");
 
-superagent.patch("https://discord.com/api/v9/users/@me")
+const userRoute = RouteBases.api + Routes.user();
+const patchBody: RESTPatchAPICurrentUserJSONBody = { avatar: `data:image/png;base64,${base64Image}` }
+
+superagent.patch(userRoute)
   .set("User-Agent", "Project Blurple Bot Image Synchronization (promise@projectblurple.com, 2.0.0)")
   .set("Authorization", `Bot ${TOKEN}`)
   .set("Content-Type", "application/json")
-  .send({ avatar: `data:image/png;base64,${base64Image}` })
-  .then(() => { console.log("Successfully updated avatar"); })
+  .send(patchBody)
+  .then(res => {
+    const body: RESTErrorData | RESTPatchAPICurrentUserResult = res.body;
+    if (typeof body === "string") return error(`Unknown response: ${body}`);
+    if ("code" in body) return error(`Error ${body.code}: ${body.message}`);
+    console.log("Successfully updated avatar");
+  })
   .catch((err: request.ResponseError) => {
-    console.error("Failed to update avatar:", err.response?.text);
-    process.exit(1);
+    error(`Failed to update avatar: ${err.response?.text}`);
   });
+
+function error(message: string) {
+  console.error(message);
+  process.exit(1);
+}
